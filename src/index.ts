bb3d4dc7fbdb58acf9c084256fb68494490cf305
moseoh <azqazq195@gmail.com>
2024-06-10 19:53:52 +0900

    chore: test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as context from './context';
 import {checkClosingIssuesReferences} from "./service/checkClosingIssuesReferences";
 import {addAssigneesPullRequestCreator} from "./service/addAssigneesPullRequestCreator";
+import {getGraphqlClient} from "./graphqlClient";
 
 async function run(): Promise<void> {
     const inputs: context.Inputs = await context.getInputs();
+
+    console.log(inputs)
 
     // PR에 연결된 Issue 확인
     await checkClosingIssuesReferences(inputs);
