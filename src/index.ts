fe21ad130bf16ac77a720ac2f47257ae05185724
moseoh <azqazq195@gmail.com>
2024-06-10 19:56:13 +0900

    chore: info 추가

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import * as context from './context';
 import {checkClosingIssuesReferences} from "./service/checkClosingIssuesReferences";
 import {addAssigneesPullRequestCreator} from "./service/addAssigneesPullRequestCreator";
 import {getGraphqlClient} from "./graphqlClient";
+import * as core from '@actions/core'
 
 async function run(): Promise<void> {
     const inputs: context.Inputs = await context.getInputs();
 
-    console.log(inputs)
+    core.startGroup("info")
+    core.info(inputs.owner)
+    core.info(inputs.repo)
+    core.info(String(inputs.prNumber))
+    core.info(inputs.token)
+    core.endGroup()
 
     // PR에 연결된 Issue 확인
     await checkClosingIssuesReferences(inputs);
